caba31f822189f627473f8c1adef09a95fd8d623
ナッティ <furytorch.1861@gmail.com>
2025-10-24 10:25:59 +0700

    add args draft

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
 import io
+import argparse
 
 
 def main():
-    print("Hello world!")
+    paser = argparse.ArgumentParser(description="CLI To-Do list that just work.")
+    paser.add_argument("-a", "--add", type=str, help="Add a new task.")
+    paser.add_argument("-c", "--complete", type=int, help="Mark task as completed.")
+    paser.add_argument("-d", "--discard", type=int, help="Discard a task.")
+    paser.add_argument(
+        "-C", "--clean", help="Clean To-Do list by remove completed and discarded task."
+    )
+
+    args = paser.parse_args()
 
 
 if __name__ == "__main__":
